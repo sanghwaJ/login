6b5e32b8fd2cdd36595d811b8a81a9f83bb68aff
정상화 <jungsh930210@gmail.com>
2023-02-04 15:23:50 +0900

    edit

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -9,13 +9,13 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig {
+
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
-
         return filterRegistrationBean;
     }
 }
--- src/main/java/hello/login/web/filter/LogFilter.java
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -11,20 +11,17 @@ import java.util.UUID;
 public class LogFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        log.info("Log Filter init");
+        log.info("log filter init");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("Log Filter doFilter");
-
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
-
         String uuid = UUID.randomUUID().toString();
-
         try {
-            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            log.info("REQUEST  [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
         } catch (Exception e) {
             throw e;
         } finally {
@@ -34,6 +31,6 @@ public class LogFilter implements Filter {
 
     @Override
     public void destroy() {
-        log.info("Log Filter destroy");
+        log.info("log filter destroy");
     }
 }
